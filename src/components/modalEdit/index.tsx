bfec6a00e9cf16680b22edcb2392b7bd1cb04890
Cézar Fernando <cezarfbc@gmail.com>
2023-07-04 15:43:59 -0300

    minutes update
    ser possivel atualizar um cadastro passando os minutos

--- src/components/modalEdit/index.tsx
+++ src/components/modalEdit/index.tsx
@@ -1,6 +1,6 @@
 import { AiOutlineClose } from "react-icons/ai"
 import { UseAuth } from "../../hooks/auth"
-import { formatISO, getHours, parseISO, setHours } from "date-fns"
+import { formatISO, getHours, getMinutes, parseISO, setHours, setMinutes } from "date-fns"
 import { useState } from "react"
 import { api } from "../../server"
 import { toast } from 'react-toastify';
@@ -12,30 +12,50 @@ interface IModal {
     name: string
     hour: string
     id: string
+    minutes: string
 }
-export function ModalEdit({isOpen, handleChangeModal, hour, name, id}: IModal) {
+export function ModalEdit({isOpen, handleChangeModal, hour, name, id, minutes}: IModal) {
     const { availableSchedules, schedules, date, handleSetDate } = UseAuth()
     const [hourSchedule, setHourSchedule] = useState('')
+    const [minuteSchedule, setMinuteSchedule] = useState('')
+    //console.log("🚀 ~ file: index.tsx:21 ~ ModalEdit ~ minuteSchedule:", minuteSchedule)
     //console.log("🚀 ~ file: index.tsx:19 ~ ModalEdit ~ hourSchedule:", hourSchedule)
 
     const currentValue = new Date().toISOString().split('T')[0]
 
-    const  filteredDate = availableSchedules.filter((hour) => {
-        const isSchedulesAvailable = !schedules.find((schedulesItem) => {
-            const scheduleDate = new Date(schedulesItem.date)
-            const scheduleHour = getHours(scheduleDate)
-            return scheduleHour === Number(hour)
-        })
-        return isSchedulesAvailable
+    // const  filteredDate = availableSchedules.filter((hour) => {
+    //     const isSchedulesAvailable = !schedules.find((schedulesItem) => {
+    //         const scheduleDate = new Date(schedulesItem.date)
+    //         const scheduleHour = getHours(scheduleDate)
+    //         const scheduleMinutes = getMinutes(scheduleDate)
+    //         return scheduleHour === Number(hour) && scheduleMinutes
+    //     })
+    //     console.log("🚀 ~ file: index.tsx:32 ~ filteredDate ~ isSchedulesAvailable:", isSchedulesAvailable)
+    //     return isSchedulesAvailable
+    // })
+    const isSchedulesAvailable = !schedules.find((schedulesItem) => {
+        const scheduleDate = new Date(schedulesItem.date)
+        const scheduleHour = getHours(scheduleDate)
+        const scheduleMinutes = getMinutes(scheduleDate)
+        return scheduleHour === Number(hour) && scheduleMinutes
     })
-    //console.log("🚀 ~ file: index.tsx:31 ~ filteredDate ~ filteredDate:", filteredDate)
-
-    const handleChangeHour = (hour: string) => {
+    
+    const handleChangeHour = (hour: string, minutes: string) => {
         setHourSchedule(hour)
+        setMinuteSchedule(minutes)
     }
 
     const updateData = async () => {
-        const formattedDate = formatISO(setHours(parseISO(date), parseInt(hourSchedule)),)
+        const formattedHoursDate = formatISO(setHours(parseISO(date), parseInt(hourSchedule)))
+        const formattedMinutesDate = formatISO(setMinutes(parseISO(date), parseInt(minuteSchedule)))
+
+        const datePart1 = formattedHoursDate.slice(0, 13);
+        const timePart2 = formattedMinutesDate.slice(13, 25);
+
+        const concatenatedDate = `${datePart1}${timePart2}`;
+        const formattedDate = concatenatedDate;
+        console.log("🚀 ~ file: index.tsx:57 ~ updateData ~ formattedDate:", formattedDate)
+
         try {     
             await api.put(`/schedules/${id}`, {
                 date: formattedDate,
@@ -58,7 +78,7 @@ export function ModalEdit({isOpen, handleChangeModal, hour, name, id}: IModal) {
                         <AiOutlineClose size={25} className="cursor-pointer" onClick={handleChangeModal} />
                     </div>
                     <div className="py-4 px-8">
-                        <p className="text-secondary text-2xl mb-3">{hour}h {name}</p>
+                        <p className="text-secondary text-2xl mb-3">{hour}:{minutes}h {name}</p>
 
                         <div className="text-primary flex justify-between mb-6 items-center">
                             <label>Indique uma nova data</label>
@@ -73,7 +93,15 @@ export function ModalEdit({isOpen, handleChangeModal, hour, name, id}: IModal) {
 
                         <div className="text-primary flex justify-between mb-6 items-center">
                             <label>Indique uma nova hora</label>
-                            <select 
+                            <input 
+                                type="time" 
+                                onChange={(e) => { 
+                                    const [hour, minutes] = e.target.value.split(":")
+                                    handleChangeHour(hour, minutes)
+                                }}
+                                className="border-solid border-[1px] border-primary rounded-[10px] w-2/5 p-1 text-primary cursor-text"
+                            />
+                            {/* <select 
                                 onChange={(e) => handleChangeHour(e.target.value)}
                                 className="border-solid border-[1px] border-primary rounded-[10px] w-2/5 p-1 text-primary cursor-pointer">
                                     <option>nova hora</option>
@@ -86,7 +114,7 @@ export function ModalEdit({isOpen, handleChangeModal, hour, name, id}: IModal) {
                                         )
                                     })
                                 }
-                            </select>
+                            </select> */}
                         </div>
                     </div>
                     <div className="flex justify-between pt-0 px-8 pb-8">
