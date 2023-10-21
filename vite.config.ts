f0070607c209e871cf9adf2abb2b9cd8e78a50d4
Cezar Fernando <czarfbc@gmail.com>
2023-10-20 21:14:58 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     port: 3003,
-  }
-})
+  },
+});
