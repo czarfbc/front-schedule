ce2d2a9541e45b5fc266cf9b07493b117943c8e0
Cezar Fernando <czarfbc@gmail.com>
2023-10-24 15:07:29 -0300

    btn

--- src/page/notFound/index.tsx
+++ src/page/notFound/index.tsx
@@ -7,7 +7,7 @@ export function NotFound() {
 
       <Link to={"/"}>
         <button className="py-3 px-4 font-semibold p-1 border-solid border-black border-2 bg-gray-50 hover:bg-opacity-80 active:shadow-[inset_-4px_4px_#222] active:text-sm shadow-[-2px_3px_0_#222,_-4px_6px_#000] rounded-md">
-          Agenda_Dia
+          Página Incial
         </button>
       </Link>
     </div>
